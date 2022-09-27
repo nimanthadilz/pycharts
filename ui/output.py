e51e38bf8a3c0d6e981e0aac46a9360f8d2fab2c
Nimantha Cooray <nimanthacooray@gmail.com>
2022-09-27 10:29:14 +0530

    Fix chart overwriting and centering issue
    When a chart is generated in the full screen mode, it was not centered.
    Further generating a second chart, overwrites the previous chart.
    These issues are fixed now by using a new canvas everytime.

--- ui/output.py
+++ ui/output.py
@@ -9,23 +9,25 @@ class Output:
         styles = ttk.Style()
         styles.configure('Output.TFrame', borderwidth=5, relief="raised")
 
-        outputFrame = ttk.Frame(root, padding="3 3 12 12", style="Output.TFrame")
-        outputFrame["borderwidth"] = 2 
-        outputFrame.grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-        outputFrame.rowconfigure(0, weight=1)
-        outputFrame.columnconfigure(0, weight=1)
+        self.outputFrame = ttk.Frame(root, padding="3 3 12 12", style="Output.TFrame")
+        self.outputFrame["borderwidth"] = 2 
+        self.outputFrame.grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.outputFrame.rowconfigure(0, weight=1)
+        self.outputFrame.columnconfigure(0, weight=1)
 
         # Create the default figure
         fig = Figure(figsize=(8, 6), dpi=100, edgecolor="black", linewidth=1)
 
         # Embed the chart
-        self.figure_canvas = FigureCanvasTkAgg(fig, master=outputFrame)
+        self.figure_canvas = FigureCanvasTkAgg(fig, master=self.outputFrame)
         self.figure_canvas.draw()
         self.figure_canvas.get_tk_widget().grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 
-    def show_chart(self, figure):
-        self.figure_canvas.figure = figure
+    def show_chart(self, figure: Figure):
+        figure.set_edgecolor("black")
+        figure.set_linewidth(1)
+        self.figure_canvas = FigureCanvasTkAgg(figure, master=self.outputFrame)
+        self.figure_canvas.get_tk_widget().grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         self.figure_canvas.draw()
-        self.figure_canvas.flush_events()
 
 
