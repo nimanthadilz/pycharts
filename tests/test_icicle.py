c071301a2dd7874674308ec03e8e8dbb512b085b
poornaSavindi <89610997+poornaSavindi@users.noreply.github.com>
2022-09-28 15:06:03 +0530

    Add unit tests for icicle chart

---
+++ tests/test_icicle.py
@@ -0,0 +1,21 @@
+import unittest
+from chart.icicle import Icicle
+
+class TestIciclemap(unittest.TestCase):
+    def setUp(self):
+        data = {
+            "Documents": (None, None),
+            "School": (None, "Documents"),
+            "Assignment": (100, "School"),
+            "Personal": (None, "Documents"),
+            "CV": (200, "Personal")
+        }
+        self.icicle = Icicle(data)
+
+    def test_get_node_value(self):
+        self.assertEqual(self.icicle._Icicle__calculate_color(1280), '#337699d', "Should equal to #337699d")
+
+if __name__ == "__main__":
+    unittest.main()
+
+
