1cae1fe7cf447160c9bb3ec3e3b115bedf836b77
Nimantha Cooray <nimanthacooray@gmail.com>
2022-09-29 23:48:06 +0530

    Rename camelcase to snakecase

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -8,15 +8,15 @@ class Sidebar:
         styles = ttk.Style()
         styles.configure('Danger.TFrame', borderwidth=5, relief="raised")
 
-        sidebarFrame = ttk.Frame(root, padding="3 3 12 12", style="Danger.TFrame")
-        sidebarFrame["borderwidth"] = 2 
-        sidebarFrame.grid(row=0, column=1, sticky=(tk.N, tk.W, tk.E, tk.S))
-        sidebarFrame.rowconfigure(1, weight=1)
-        sidebarFrame.columnconfigure(0, weight=1)
+        sidebar_frame = ttk.Frame(root, padding="3 3 12 12", style="Danger.TFrame")
+        sidebar_frame["borderwidth"] = 2 
+        sidebar_frame.grid(row=0, column=1, sticky=(tk.N, tk.W, tk.E, tk.S))
+        sidebar_frame.rowconfigure(1, weight=1)
+        sidebar_frame.columnconfigure(0, weight=1)
 
-        nameLabel = ttk.Label(sidebarFrame, text="Sidebar")
+        nameLabel = ttk.Label(sidebar_frame, text="Sidebar")
         nameLabel.grid(row=0, column=0, sticky=W)
 
-        DataInputFrame(sidebarFrame, app)
+        DataInputFrame(sidebar_frame, app)
 
 
