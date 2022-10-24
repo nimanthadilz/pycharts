fab1b1ca742b73c2314a7886e7eae3be40d3b1f6
poornaSavindi <89610997+poornaSavindi@users.noreply.github.com>
2022-10-25 05:24:14 +0530

    Add chart font cutomization functionality

--- chart/icicle.py
+++ chart/icicle.py
@@ -6,10 +6,10 @@ import sys
 
 class Icicle(BaseChart):
 
-    def __init__(self, data, chart_properties = {}):
+    def __init__(self, data, chart_properties={}):
         super().__init__(data)
+        self.data_set = self.__duplicate_object(self.data)
         self.__configure_chart()
-        self.draw_chart()
 
         if chart_properties:
             self.chart_properties = chart_properties
@@ -20,15 +20,25 @@ class Icicle(BaseChart):
                 "chart_font_size": 8
             }
 
+        self.draw_chart()
+
         self.__customize_chart()
+
+    def __duplicate_object(self, dict):
+        dup_dict = {}
+        for key in list(dict.keys()):
+            dup_dict[key] = dict[key]
+
+        return dup_dict
 
     def __customize_chart(self):
         if self.chart_properties:
             if "title" in self.chart_properties and self.chart_properties["title"]:
                 plt.title(self.chart_properties["title"])
-            if self.chart_properties["title"] and self.chart_properties["title_font_family"] and self.chart_properties["title_font_size"]:
-                plt.title(self.chart_properties["title"], fontsize=self.chart_properties["title_font_size"], fontfamily=self.chart_properties["title_font_family"])
-
+            if self.chart_properties["title"] and self.chart_properties["title_font_family"] and self.chart_properties[
+                "title_font_size"]:
+                plt.title(self.chart_properties["title"], fontsize=self.chart_properties["title_font_size"],
+                          fontfamily=self.chart_properties["title_font_family"])
 
     def __configure_chart(self, left_color="#8B008B", right_color="#FF00FF", element_width=1.5,
                           chart_height=20):
@@ -62,21 +72,21 @@ class Icicle(BaseChart):
         return self.figure
 
     def __get_updated_value(self, keys, current_parent, current_node):
-        return self.data[keys[keys.index(current_parent)]][0] + current_node[0] if \
-            self.data[keys[keys.index(current_parent)]][0] != None else current_node[0]
+        return self.data_set[keys[keys.index(current_parent)]][0] + current_node[0] if \
+            self.data_set[keys[keys.index(current_parent)]][0] != None else current_node[0]
 
     def __convert_data(self):
         '''
             calculates the overall values for every key in the data dictionary
         '''
-        keys = list(self.data.keys())
+        keys = list(self.data_set.keys())
         self._max = -1000000000000000000
         self._min = sys.maxsize
 
         # update the value attribute in the tuple of parent elements
         for i in range(1, len(keys)):
-            current_parent = self.data[keys[i]][1]
-            current_node = self.data[keys[i]]
+            current_parent = self.data_set[keys[i]][1]
+            current_node = self.data_set[keys[i]]
 
             while current_parent != None:
                 if current_node[0] != None:
@@ -85,13 +95,13 @@ class Icicle(BaseChart):
 
                     updated_value = self.__get_updated_value(keys, current_parent, current_node)
 
-                    self.data[keys[keys.index(current_parent)]] = (
-                        updated_value, self.data[keys[keys.index(current_parent)]][1])
+                    self.data_set[keys[keys.index(current_parent)]] = (
+                        updated_value, self.data_set[keys[keys.index(current_parent)]][1])
 
-                current_parent = self.data[current_parent][1]
+                current_parent = self.data_set[current_parent][1]
 
-        self._max = self.data[keys[0]][0] if self._max < self.data[keys[0]][0] else self._max
-        self._min = self.data[keys[0]][0] if self._min > self.data[keys[0]][0] else self._min
+        self._max = self.data_set[keys[0]][0] if self._max < self.data_set[keys[0]][0] else self._max
+        self._min = self.data_set[keys[0]][0] if self._min > self.data_set[keys[0]][0] else self._min
 
     def __calculate_color(self, value):
         '''
@@ -131,18 +141,18 @@ class Icicle(BaseChart):
         # iterating through the list to find the parent
         for i in range(index - 1, -1, -1):
             # parent has other children
-            if self.data[keys[i]][1] == item[1]:
-                height = self.data[self.data[keys[i]][1]][5] * (
-                        item[0] / self.data[self.data[keys[i]][1]][0])
-                start_x = self.data[keys[i]][2]
-                start_y = self.data[keys[i]][3] - height
+            if self.data_set[keys[i]][1] == item[1]:
+                height = self.data_set[self.data_set[keys[i]][1]][5] * (
+                        item[0] / self.data_set[self.data_set[keys[i]][1]][0])
+                start_x = self.data_set[keys[i]][2]
+                start_y = self.data_set[keys[i]][3] - height
                 break
 
         # current elem is the first or sole elem
         if start_x == start_y == -1:
-            height = self.data[item[1]][5] * (item[0] / self.data[item[1]][0])
-            start_x = self.data[item[1]][2] + self._width
-            start_y = self.data[item[1]][3] + self.data[item[1]][5] - height
+            height = self.data_set[item[1]][5] * (item[0] / self.data_set[item[1]][0])
+            start_x = self.data_set[item[1]][2] + self._width
+            start_y = self.data_set[item[1]][3] + self.data_set[item[1]][5] - height
 
         return height, start_x, start_y
 
@@ -150,9 +160,9 @@ class Icicle(BaseChart):
         '''
             draws the rest of the rectangles in the plot other than the root rectangle
         '''
-        for e in self.data:
+        for e in self.data_set:
             if e != keys[0]:
-                item = self.data[e]
+                item = self.data_set[e]
                 height = 20
 
                 height, start_x, start_y = self.__get_parent_nodes(index, keys, item)
@@ -164,9 +174,11 @@ class Icicle(BaseChart):
                 self.__add_rectangle(start_x, start_y, self._width, height, color)
 
                 # update the main array with coordinates
-                self.data[keys[index]] = [item[0], item[1], start_x, start_y, self._width, height]
+                self.data_set[keys[index]] = [item[0], item[1], start_x, start_y, self._width, height]
                 # add text into the rectangle
-                self.ax.text(start_x + self._width / 3, start_y + height / 2, keys[index], color='white', fontsize=8)
+                self.ax.text(start_x + self._width / 3, start_y + height / 2, keys[index],
+                             color='white', fontsize=self.chart_properties["chart_font_size"],
+                             fontfamily=self.chart_properties["chart_font_family"])
 
                 index += 1
 
@@ -176,16 +188,16 @@ class Icicle(BaseChart):
         '''
         self.__convert_data()
         self.__configure_plot()
-        keys = list(self.data.keys())
+        keys = list(self.data_set.keys())
 
         # add rectangle to plot for the first root parent
-        color = self.__calculate_color(self.data[keys[0]][0])
+        color = self.__calculate_color(self.data_set[keys[0]][0])
         self.__add_rectangle(0, 0, self._width, self._maxHeight, color)
 
         self.ax.text(self._width / 3, self._maxHeight / 2, keys[0], color='white', fontsize=8)
 
         # update the list with width and height of the element
-        self.data[keys[0]] = (self.data[keys[0]][0], self.data[keys[0]][1], 0, 0, 2, 20)
+        self.data_set[keys[0]] = (self.data_set[keys[0]][0], self.data_set[keys[0]][1], 0, 0, 2, 20)
 
         index = 1
 
